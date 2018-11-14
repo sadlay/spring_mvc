875b7e230e832564afa7e8e60c28212cfd66d20b
Lay <1778038380@qq.com>
2018-11-14 20:15:11 +0800

    上传文件

---
+++ src/main/java/com/lay/mvc/controller/FileController.java
@@ -0,0 +1,60 @@
+package com.lay.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.rmi.server.ExportException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Description:
+ * @Author: lay
+ * @Date: Created in 19:55 2018/11/14
+ * @Modified By:IntelliJ IDEA
+ */
+@Controller
+public class FileController {
+    @GetMapping("/upload/page")
+    public String uploadPage(){
+        return "/file/upload";
+    }
+    @PostMapping("/upload/request")
+    @ResponseBody
+    public Map<String,Object> uploadRequest(HttpServletRequest request){
+        boolean flag=false;
+        MultipartHttpServletRequest mreq=null;
+        //强制转换为MultipartHttpServletRequest接口对象
+        if(request instanceof MultipartHttpServletRequest){
+            mreq=(MultipartHttpServletRequest)request;
+        }else {
+            return dealResultMap(false,"上传失败");
+        }
+        //获取MultipartFile文件
+        MultipartFile mf=mreq.getFile("file");
+        //获取源文件名称
+        String fileName=mf.getOriginalFilename();
+        File file=new File(fileName);
+        try {
+            //保存文件
+            mf.transferTo(file);
+        }catch (Exception e){
+            e.printStackTrace();
+            return dealResultMap(false,"上传失败");
+        }
+        return dealResultMap(true,"上传成功");
+    }
+
+    private Map<String,Object> dealResultMap(boolean success,String msg){
+        Map<String,Object> result=new HashMap<>();
+        result.put("success",success);
+        result.put("msg",msg);
+        return result;
+    }
+}
